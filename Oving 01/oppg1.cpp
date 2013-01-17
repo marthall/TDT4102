a450bd3ce4f1341d5941cf755576b68e2dd755e3
marthall <marthall@outlook.com>
2013-01-17 16:38:54 +0100

    cleaned up

--- Oving 01/oppg1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-// Dette er et helt enkelt program som du kan kopiere og bruke i denne øvingen.
-using namespace std;
-
-int main() {
-    cout << "Hello World!" << endl;
-    return 0;
-}
