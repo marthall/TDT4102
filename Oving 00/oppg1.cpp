2d06cd179994c6cee835c8277c7a6f580da5fc66
marthall <marthall@outlook.com>
2013-01-17 16:36:09 +0100

    initial commit

---
+++ Oving 00/oppg1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// Dette er et helt enkelt program som du kan kopiere og bruke i denne øvingen.
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
