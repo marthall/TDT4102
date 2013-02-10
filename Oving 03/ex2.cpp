f4a4879a1d39ff043406d2544d0f98cae9c297d1
marthall <marthall@outlook.com>
2013-02-10 20:59:50 +0100

    added steps

--- Oving 03/ex2.cpp
+++ Oving 03/ex2.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
-void printNumbers(int weekdays[], int size);
+void printNumbers(int weekdays[][7], int size);
+void randomizeList(int weekdays[][7], int size, int antallOpprigninger);
+int countCalls(int weekdays[][7], int size, int weekdayIndex);
 
 int weekdays[5][7];
 int size = sizeof(weekdays)/sizeof(weekdays[0]);
+int antallOpprigninger = 1000;
+int weekdayIndex = 3;
 
 int main() {
-	printNumbers(weekdays[]*, size);
+	srand(time(NULL));
+	randomizeList(weekdays, size, antallOpprigninger);
+	printNumbers(weekdays, size);
+	cout << "Antall opprigninger: " << countCalls(weekdays, size, weekdayIndex) << endl;
 	return 0;
 }
 
-void printNumbers(int weekdays[]*, int size) {
-	for (int i = 0; i < size; ){
+void printNumbers(int weekdays[][7], int size) {
+	for (int i = 0; i < size; i++){
 		for (int j = 0; j < 7; j++) {
-			cout << weekdays << "\t";
+			cout << weekdays[i][j] << "\t";
 		}
-		cout << "\n"
+		cout << "\n";
 	}
+}
+
+void randomizeList(int weedays[][7], int size, int antallOpprigninger){
+	for(int i = 0; i < antallOpprigninger; i++){
+		int randY = rand() % 7;
+		int randX = rand() % size;
+		weekdays[randX][randY]++;
+	}
+}
+
+int countCalls(int weekdays[][7], int size, int weekdayIndex){
+	int sumOpprigninger = 0;
+	for(int i = 0; i < size; i++){
+		sumOpprigninger += weekdays[i][weekdayIndex];
+	}
+	return sumOpprigninger;
 }
